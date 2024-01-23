33858f720a40ad24a656a5b981dc8ae8fceb5c2f
Leandro Amaral <lramaral02m@gmail.com>
2024-01-23 12:47:19 -0300

    Conectando com o banco

--- ImportaPlanilhaExcel/Controllers/HomeController.cs
+++ ImportaPlanilhaExcel/Controllers/HomeController.cs
@@ -1,15 +1,24 @@
-﻿using ImportaPlanilhaExcel.Models;
+﻿using ImportaPlanilhaExcel.Data;
+using ImportaPlanilhaExcel.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace ImportaPlanilhaExcel.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _context;
 
-        public IActionResult Index()
+        public HomeController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var produtos = await _context.Produtos.ToListAsync();
+            return View(produtos);
         }
 
     }
