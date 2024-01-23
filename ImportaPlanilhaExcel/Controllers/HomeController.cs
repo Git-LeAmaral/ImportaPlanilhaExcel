a1691eba5ff163d900a1c74261269f15ac536001
Leandro Amaral <lramaral02m@gmail.com>
2024-01-22 23:25:49 -0300

    Adicionar arquivos de projeto.

---
+++ ImportaPlanilhaExcel/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using ImportaPlanilhaExcel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ImportaPlanilhaExcel.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ImportaPlanilhaExcel/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ImportaPlanilhaExcel.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImportaPlanilhaExcel.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ProdutoModel> Produtos { get; set; }
+
+    }
+}
